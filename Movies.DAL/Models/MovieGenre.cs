afdc70a597f3107199f5a7b325aecc141bcc21c8
Hoang Long <hoanglong.cmb@gmail.com>
2024-05-14 09:37:22 +0700

    add database

---
+++ Movies.DAL/Models/MovieGenre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.DAL.Models
+{
+    public class MovieGenre
+    {
+        [Key]
+        public int Id { get; set; }
+        public int MovieID { get; set; }
+        public virtual Movie Movie { get; set; }
+        public int GenreID { get; set; }
+        public virtual Genre Genre { get; set; }
+    }
+}
