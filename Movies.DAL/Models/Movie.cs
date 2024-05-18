491d3946f11a83b699bed92c5aaa44ee342a8a84
Hoang Long <hoanglong.cmb@gmail.com>
2024-05-18 23:33:14 +0700

    update

--- Movies.DAL/Models/Movie.cs
+++ Movies.DAL/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +12,24 @@ namespace Movies.DAL.Models
     public class Movie
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
+        [Column(TypeName = "NVARCHAR")]
+        [StringLength(500)]
         public string Title { get; set; }
+        [Column(TypeName = "NVARCHAR")]
+        [StringLength(3000)]
         public string? Description { get; set; }
         public string? StreamURL { get; set; }
         public double RentalPrice { get; set; }
         public double SalePrice { get; set; }
-        public byte Status { get; set; }
+        [Column(TypeName = "NVARCHAR")]
+        [StringLength(30)]
+        public string Status { get; set; }
+        public double Bugget { get; set; }
+        public string BackdropURL { get; set; }
+        public string PosterURL { get; set; }
+
 
         public virtual ICollection<MovieGenre> MovieGenres { get; set; }
         public virtual ICollection<WhishList> WhishLists { get; set; }
