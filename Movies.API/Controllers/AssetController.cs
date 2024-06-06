0a08310a33c38025f43c0bced4c13ad15686aa7b
bohoplatao <cuhopyi2004@gmail.com>
2024-06-06 16:50:48 +0700

    test

--- Movies.API/Controllers/AssetController.cs
+++ Movies.API/Controllers/AssetController.cs
@@ -8,40 +8,6 @@ namespace Movies.API.Controllers
     [ApiController]
     public class AssetController : ControllerBase
     {
-        [HttpGet("GetUserById")]
-        public User GetUserByID(Guid id)
-        {
-            var context = new DAL.Context.MovieDbContext();
-            return context.Users.Find(id);
-        }
-
-        [HttpGet("GetAllUser")]
-        public  ICollection<User> GetAllUser()
-        {
-            var context = new DAL.Context.MovieDbContext();
-            var user = context.Users.ToList();
-            return user;
-        }
-
-        [HttpPost("Create-User")]
-        public ActionResult CreateUser(User user)
-        {
-            try
-            {
-                var context = new DAL.Context.MovieDbContext();
-                context.Users.Add(user);
-                context.SaveChanges();
-                return Ok();
-            }
-            catch (Exception)
-            {
-
-                return BadRequest();
-            }
-        }
-
-
-
 
 
     }
